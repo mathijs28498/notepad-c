32e7d84fd6b8b46029fac3727b115fe6b0c47e98
Mathijs Frank <33691132+mathijs28498@users.noreply.github.com>
2026-01-08 03:55:35 +0000

    Finished working src_test

--- src_test/main.c
+++ src_test/main.c
@@ -3,8 +3,27 @@
 #include <test_lib.h>
 #include <test_lib_i.h>
 
+#include <vulkan/vulkan.h>
+#include <cglm.h>
+
 int main(int argc, const char *argv[])
 {
+    vec2 v0 = {1.0, 2.0}; 
+    vec2 v1 = {1.0, 5.0}; 
+    // glm_vec2((float[]){1.0, 1.0}, v);
+    float cross = glm_vec2_cross(v0, v1);
+
+    // glm_vec2_print(v, stdout);
+    printf("cross: %f\n", cross);
+
+    VkApplicationInfo appInfo = {0};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
     TestStruct test_struct = {
         .test_int = 400};
 
